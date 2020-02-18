c95f7fee18bc9a3c2662e9ff7575fc78bb86b630
Zsolt Sándor <zsolt.sandor@gmail.com>
2020-02-18 20:34:32 +0100

    removed unused class

--- src/ports/http/AppModule.ts
+++ src/ports/http/AppModule.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CatsController } from './CatsController';
 import { GetUserController } from '../../domain/useCases/getUser/GetUserController';
 import { InMemoryUserRepository } from '../../domain/repository/InMemoryUserRepository';
 import { GetUserUseCase } from '../../domain/useCases/getUser/GetUserUseCase';
 
 @Module({
   imports: [],
-  controllers: [CatsController, GetUserController],
+  controllers: [GetUserController],
   providers: [
     {provide: 'GetUserUseCase', useClass: GetUserUseCase},
     {provide: 'UserRepository', useClass: InMemoryUserRepository}
